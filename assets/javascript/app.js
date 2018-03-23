fd0c8c275311e4ef4f2584b6b80116ae4913c148
Robert Paul Trettenero <paul.trettenero@petrocloud.com>
2018-03-23 18:12:00 -0500

    Replaced the table population code from Jquery to vue.js this got decklist working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,4 @@
-    // The below code fills in the first row of the table
-    //var type = "fight+club";
     var queryURL = "https://api.hearthstonejson.com/v1/22611/enUS/cards.collectible.json";
-    var firstc = 0;         //these are global counters used in one of out later functions
-    var secondc = 1;
-    var thirdc = 2;
 
     $.ajax({
       url: queryURL,
@@ -13,72 +8,58 @@
       //Here's where the function will go that fill the array appropiatly...
       var standdruid = getSet("DRUID", response);
       console.log(standdruid);
-      filltable(standdruid);                //change this from response to a more concise array
 
-      //console.log(response);
-      
+      new Vue({
+        el:"#vue-app",
+        data:{
+          deckList: '',
+          name1: 'Press Select to start',       //Could start with these empty and make seperate 9 buttons for the claz
+          name2: 'Press Select to start',
+          name3: 'Press Select to start'
+        },
+        methods:{
+
+          populate(){
+            var len = standdruid.length;
+            var randomarr = [];
+
+            for(var i = 0; i < 90; i++){
+               randomarr.push(Math.floor(Math.random() * len));
+            }
+
+            this.name1 = standdruid[randomarr[0]].name;
+            this.name2 = standdruid[randomarr[1]].name;
+            this.name3 = standdruid[randomarr[2]].name;
+
+            this.stats1 = standdruid[randomarr[0]].cost + " mana " + standdruid[randomarr[0]].rarity + " " + standdruid[randomarr[0]].type;
+            this.stats2 = standdruid[randomarr[1]].cost + " mana " + standdruid[randomarr[0]].rarity + " " + standdruid[randomarr[0]].type;
+            this.stats3 = standdruid[randomarr[2]].cost + " mana " + standdruid[randomarr[0]].rarity + " " + standdruid[randomarr[0]].type;
+
+            this.text1 = standdruid[randomarr[0]].text;
+            this.text2 = standdruid[randomarr[1]].text;
+            this.text3 = standdruid[randomarr[2]].text;
+          },
+          //I couldn't figure out how to call "append" passing in a  parameter.. Was giving me weird output.. So I went redundant
+          append1(){
+            this.deckList += '<br>' + this.name1;
+            this.populate();
+          },
+
+          append2(){
+            this.deckList += '<br>' + this.name2;
+            this.populate();
+          },
+
+          append3(){
+            this.deckList += '<br>' + this.name3;
+            this.populate();
+          }
+
+        }
+      });
     });
 
     //-------------------------function definitions-------------------------
-    function filltable(response){
-
-      var len = response.length;
-      var randomarr = [];
-
-      for(var i = 0; i < 90; i++){
-         randomarr.push(Math.floor(Math.random() * len));
-      }
-
-        // Getting the first row of the table
-      var firstRowTds = $("table")
-        .children()
-        .eq(1)          //choose tbody
-        .children("tr")
-        .eq(0)          //choose the first tr
-        .children("td");
-
-
-      // Setting the inner text of each td in the first row
-      firstRowTds.eq(0).html('<button class="btn waves-effect waves-light" type="submit" name="action">'+response[randomarr[0+firstc]].name+'</button>');
-      //The use of randomarr[0+firstc] is simply a way to get one of 90 random numbers between 0 and len
-      firstRowTds.eq(1).text(response[randomarr[0+firstc]].cost + " mana " + response[randomarr[0+firstc]].rarity + " " +  response[randomarr[0+firstc]].type);  //should do attack and health for minions here
-
-      firstRowTds.eq(2).html($.parseHTML(response[randomarr[0+firstc]].text));
-
-      // Getting the second row of the table
-      var secondRowTds = $("table")
-        .children()
-        .eq(1)          //choose tbody
-        .children("tr")
-        .eq(1)          //choose the second tr
-        .children("td");
-
-      // Setting the inner text of each td in the second row
-      //gets the card's name
-      secondRowTds.eq(0).html('<button class="btn waves-effect waves-light" type="submit" name="action">'+response[randomarr[0+secondc]].name+'</button>');
-
-      secondRowTds.eq(1).text(response[randomarr[0+secondc]].cost + " mana " + response[randomarr[0+secondc]].rarity + " " + response[randomarr[0+secondc]].type);  //should do attack and health for minions here too
-
-      //gets the description of the card
-      secondRowTds.eq(2).html($.parseHTML(response[randomarr[0+secondc]].text));
-
-      // Getting the third row of the table
-      var thirdRowTds = $("table")
-        .children()
-        .eq(1)          //choose tbody
-        .children("tr")
-        .eq(2)          //choose the third tr
-        .children("td");
-
-      // Setting the inner text of each td in the second row
-      //gets the card's name
-      thirdRowTds.eq(0).html('<button class="btn waves-effect waves-light" type="submit" name="action">'+response[randomarr[0+thirdc]].name+'</button>');
-
-      thirdRowTds.eq(1).text(response[randomarr[0+thirdc]].cost + " mana " + response[randomarr[0+thirdc]].rarity + " " +  response[randomarr[0+thirdc]].type);  //should do attack and health for minions here too
-
-      //gets the description of the card
-      thirdRowTds.eq(2).html($.parseHTML(response[randomarr[0+thirdc]].text));
-    }
 
     function getSet(claz, allarr){           //This function loops through all 1500 cards 
                                             //and sets a new array of only the standard druid cards
